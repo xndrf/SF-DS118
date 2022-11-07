c2ed09623bde1ffc1808a46455a35b5fafb51e9d
xndrf <117300285+xndrf@users.noreply.github.com>
2022-11-07 19:34:03 +0300

    game_v2 edit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -11,10 +11,10 @@ def random_predict (number:int=1) -> int:
     """
     count = 0
     while True:
-        count +=1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        count += 1
+        predict_number = np.random.randint(1, 101)  #  предполагаемое число
         if number == predict_number:
-            break  # выход из цикла, если угадали
+            break  #  выход из цикла, если угадали
     return(count)
 
 print(f'Количество попыток: {random_predict()}')
@@ -30,7 +30,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # занадали список числе
+    random_array = np.random.randint(1, 101, size=(1000)) # задали список чисел
     for number in random_array:
         count_ls.append(random_predict(number))
         
