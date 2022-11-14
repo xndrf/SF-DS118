69c17ba13835fe773c7e55da5a054c143deb2026
xndrf <413x4ndr_fd5@mail.ru>
2022-11-14 18:31:04 +0300

    PY_8_Final_Task

--- project_0/PY_8_Final_Task.py
+++ project_0/PY_8_Final_Task.py
@@ -46,5 +46,6 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
-score_game(random_predict)
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
