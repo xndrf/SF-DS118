494083edcb265a6195793b56bcb4af84b3f5534d
xndrf <413x4ndr_fd5@mail.ru>
2022-12-05 18:45:04 +0300

    lesson 05/12/2022

--- lesson/draft.py
+++ lesson/draft.py
@@ -1,45 +1,25 @@
-#from collections import deque
-
-#temps = [20.6, 19.4, 19.0, 19.0, 22.1,
-     #   22.5, 22.8, 24.1, 25.6, 27.0,
-      #  27.0, 25.6, 26.8, 27.3, 22.5,
-       # 25.4, 24.4, 23.7, 23.6, 22.6,
-       # 20.4, 17.9, 17.3, 17.3, 18.1,
-        #20.1, 22.2, 19.8, 21.3, 21.3,
-        #21.9]
-#days = deque(maxlen=7)
- 
-#for temp in temps:
-    # Добавляем температуру в очередь
- #   days.append(temp)
-    # Если длина очереди оказалась равной максимальной длине очереди (7),
-    # печатаем среднюю температуру за последние 7 дней
-  #  if len(days) == days.maxlen:
-   #     print(round(sum(days) / len(days), 2), end='\n')
-# Напечатаем пустую строку, чтобы завершить действие параметра
-# end. Иначе следующая строка окажется напечатанной на предыдущей
-#print("")
-
-import numpy as np
-np.random.seed(2021)
-simple = np.random.rand()
-print(simple)
-randoms  = np.random.uniform(-150, 2021, size = 120)
-print(randoms)
-table = np.random.randint(1, 101, size=(3,2))
-print (table)
-
-even = np.arange(2, 17, 2)
-print(even)
-mix = even
-print(np.random.shuffle(mix))
-print(mix)
-select = np.random.choice(even, size=3, replace=False)
-print(select)
-triplet = np.random.permutation(select)
-print(triplet)
-
-print(np.mean([19, 242, 14, 152, 142, 1000]))
 import pandas as pd
-print(pd.__version__)
-print(pd.__name__)
+
+
+countries_df = pd.DataFrame(
+    data = [
+        ['Англия', 56.29, 133396],
+        ['Канада', 38.05, 9984670],
+        ['США', 322.28, 9826630],
+        ['Россия', 146.24, 17125191],
+        ['Украина', 45.5, 603628],
+        ['Беларусь', 9.5, 207600],
+        ['Казахстан', 17.04, 2724902]
+    ],
+    columns= ['country', 'population', 'square'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ']
+)
+print(countries_df)
+ 
+# Рассмотрим разницу в результатах работы методов в зависимости от параметра axis на примере использования метода DataFrame mean() — вычисление среднего по таблице.
+print('Считаем среднее по строкам(axis = 0) в каждом столбце:')
+
+countries_df.mean(axis=0)
+print()
+print('Считаем среднее по столбцам(axis = 1) в каждой строке')
+countries_df.mean(axis=1)
